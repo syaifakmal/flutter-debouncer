a71af144a00b026794bd0ed765f5bedd41c94789
Syaif Akmal <syaiff.sa@gmail.com>
2023-07-19 23:11:41 +0700

    Fix Documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,11 @@
+## [1.0.2] - [19 Jul 2023]
+
+* Add Documentation
+
 ## [1.0.1] - [19 Jul 2023]
 
 * Fix homepage not accessible
   
 ## [1.0.0] - [19 Jul 2023]
 
-* Add Debuncer class
+* Add Debouncer class
--- lib/src/debouncer.dart
+++ lib/src/debouncer.dart
@@ -1,9 +1,23 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 
+/// The `Debouncer` class provides a mechanism to debounce function calls,
+/// ensuring that the function is only invoked after a specified duration of
+/// inactivity.
 class Debouncer {
   Timer? _timer;
 
+  /// Debounces the provided [callback] function by canceling any existing
+  /// timer and scheduling a new timer to invoke the callback after the
+  /// specified [duration] of inactivity.
+  ///
+  /// The [callback] function is invoked only once, even if this method is
+  /// called multiple times within the [duration].
+  ///
+  /// If an [error] occurs during the debounced callback execution, it will be
+  /// caught and printed to the console in debug mode, along with the
+  /// associated [stackTrace]. In release mode, the error will not be printed
+  /// but will be silently ignored.
   void debounce(Duration duration, Function() callback) {
     _timer?.cancel();
     _timer = Timer(duration, () {
@@ -19,7 +33,12 @@ class Debouncer {
     });
   }
 
+  /// Cancels any pending debounced callback.
+  ///
+  /// If there is an active timer, it will be canceled, and the callback
+  /// associated with that timer will not be invoked.
   void cancel() {
     _timer?.cancel();
   }
 }
+
